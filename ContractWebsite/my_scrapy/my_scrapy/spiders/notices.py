116848c9984bfece53850e7ffbb87337270be07c
Denislav <deni_9000_1990@abv.bg>
2022-04-28 20:56:55 +0300

    some tries with scrapy

--- ContractWebsite/my_scrapy/my_scrapy/spiders/notices.py
+++ ContractWebsite/my_scrapy/my_scrapy/spiders/notices.py
@@ -1,50 +1,48 @@
 import json
 
 import scrapy
+from  scrapy import FormRequest
 
 
-class NoticesSpider(scrapy.Spider):
-    #user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36"
-    #coockie="isCompact=true; culture=en-US; _HttpSessionID=35E93C04EB154DDBB374AACA43484AD2; sysNoticeTypeIds=null"
-    #handle_httpstatus_list = [404, 403, 400]
-    name = 'notices'
-    allowed_domains = ['e-licitatie.ro']
-    #allowed_domains = ['quotes.toscrape.com']
-    #start_urls = ['https://www.e-licitatie.ro/views/common/notices/common/service.min.js?_=1651054814609']
-    #start_urls = ['https://quotes.toscrape.com/api/quotes?page=1']
-    start_urls=['http://www.e-licitatie.ro/api-pub/NoticeCommon/GetCNoticeList/']
-
-
-    def parse(self, response,**kwargs):
-        #strrr=response.css("div.u-items-list__item_properties::text").extract()
-        #strrr=response.css('span.ng-binding::text').extract()
-        #strrr=response.xpath('/html/body/div[1]/div/div[2]/div/div[5]/div[9]/div[2]/div[1]/div').extract()
-        print()
-        print()
-        print()
-        #strrr=response.css('#container-sizing > div.u-items-list > div.u-items-list__content').getall()
-        #strrr=response.css('.u-items-list__content').extract()
-        #strrr=response.xpath('//*[@id="container-sizing"]/div[9]/div[2]').get()
-        #date=json.loads(response.body)
-        print(response.body)
-        #strrr=date
-        #print(f"=====D E N I S L A V==={str(strrr)}")
-        print()
-        print()
-        print()
-
-        # with open("test.txt", "w") as outfile:
-        #     outfile.write(str(strrr))
-        # # #pass
-        # yield {'=====D E N I S L A V':str(strrr)}
-
-        # for title in response.css('.u-items-list__content'):
-        #     dict_ = {'title': title.css('::text').get()}
-        #     print(f"this: {dict_['title']}")
-        #     yield {'title': title.css('::text').get()}
-
-
-
+# class NoticesSpider(scrapy.Spider):
+#     #user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36"
+#     #coockie="isCompact=true; culture=en-US; _HttpSessionID=35E93C04EB154DDBB374AACA43484AD2; sysNoticeTypeIds=null"
+#     #handle_httpstatus_list = [404, 403, 400]
+#     name = 'notices'
+#     allowed_domains = ['e-licitatie.ro']
+#     #allowed_domains = ['quotes.toscrape.com']
+#     #start_urls = ['https://www.e-licitatie.ro/views/common/notices/common/service.min.js?_=1651054814609']
+#     #start_urls = ['https://quotes.toscrape.com/api/quotes?page=1']
+#     start_urls=['http://www.e-licitatie.ro/api-pub/NoticeCommon/GetCNoticeList/']
+#
+#
+#     def parse(self, response,**kwargs):
+#         #strrr=response.css("div.u-items-list__item_properties::text").extract()
+#         #strrr=response.css('span.ng-binding::text').extract()
+#         #strrr=response.xpath('/html/body/div[1]/div/div[2]/div/div[5]/div[9]/div[2]/div[1]/div').extract()
+#         print()
+#         print()
+#         print()
+#         #strrr=response.css('#container-sizing > div.u-items-list > div.u-items-list__content').getall()
+#         #strrr=response.css('.u-items-list__content').extract()
+#         #strrr=response.xpath('//*[@id="container-sizing"]/div[9]/div[2]').get()
+#         #date=json.loads(response.body)
+#         print(response.body)
+#         #strrr=date
+#         #print(f"=====D E N I S L A V==={str(strrr)}")
+#         print()
+#         print()
+#         print()
+#
+#         # with open("test.txt", "w") as outfile:
+#         #     outfile.write(str(strrr))
+#         # # #pass
+#         # yield {'=====D E N I S L A V':str(strrr)}
+#
+#         # for title in response.css('.u-items-list__content'):
+#         #     dict_ = {'title': title.css('::text').get()}
+#         #     print(f"this: {dict_['title']}")
+#         #     yield {'title': title.css('::text').get()}
 
 # class NoticesSpider(scrapy.Spider):
 #     name = "notices"
@@ -85,12 +83,9 @@ class NoticesSpider(scrapy.Spider):
 #         # with open(filename, 'wb') as f:
 #         #     f.write(response.body)
 #         # self.log(f'Saved file {filename}')
-
-
-
-from scrapy.http import request
-from scrapy.http.request.form import FormRequest
-from scrapy.http import FormRequest
+# from scrapy.http import request
+# from scrapy.http.request.form import FormRequest
+# from scrapy.http import FormRequest
 
 
 # class CodeSpider(scrapy.Spider):
@@ -108,3 +103,104 @@ from scrapy.http import FormRequest
 #
 #     def parse(self, response,**kwargs):
 #         yield json.loads(response.text)
+
+
+""" 28.04.2022"""
+# class NoticesSpider(scrapy.Spider):
+#     name = 'notices'
+#     #allowed_domains = ['e-licitatie.ro']
+#     #start_urls=['http://www.e-licitatie.ro/api-pub/NoticeCommon/GetCNoticeList/']
+#
+#     def start_requests(self):
+#         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36'}
+#         cookies = {'cookie-key': 'culture=en-US; isCompact=true; _HttpSessionID=5BF1B10EBD394BD8B5B99ED472BD854E; sysNoticeTypeIds=null'}
+#         yield scrapy.Request(
+#             url='http://www.e-licitatie.ro/api-pub/NoticeCommon/GetCNoticeList/',
+#             method='POST',
+#             headers=headers,
+#             cookies=cookies,
+#             callback=self.parse,
+#         )
+#
+#     def parse(self, response,**kwargs):
+#
+#         print("S T A R T")
+#         print("S T A R T")
+#         print("S T A R T")
+#         print("S T A R T")
+#         print()
+#         print()
+#         print(response.body)
+#         print()
+#         print()
+#         print()
+
+
+# class NoticeSpider(scrapy.Spider):
+#     name = 'notices'
+#     # allowed_domains = ['e-licitatie.ro']
+#     #start_urls=['http://www.e-licitatie.ro/api-pub/NoticeCommon/GetCNoticeList/']
+#     start_urls=['http://www.e-licitatie.ro/pub/notices/contract-notices/list/2/1']
+#
+#     def parse(self, response, **kwargs):
+#
+#         url='http://www.e-licitatie.ro/api-pub/NoticeCommon/GetCNoticeList/'
+#         my_value=response.css('strong.ng-binding').get()
+#         #my_value=response.xpath('/html/body/div[1]/div/div[2]/div/div[5]/div[9]/div[2]/div[1]/div/div/div[2]/h2/a/span').extract()
+#         yield {'=========D E N I S L A V':my_value}
+
+
+
+
+class NoticeSpider(scrapy.Spider):
+    name = "notices"
+    start_urls = ["http://www.e-licitatie.ro/pub/notices/contract-notices/list/2/1"]
+    headers = {
+    'Accept': 'application / json, text / plain, * / *',
+    'Accept - Encoding': 'gzip, deflate',
+    'Accept - Language': 'en - US, en;q = 0.9',
+    'Authorization': 'Bearer null',
+    'Cache - Control': 'no - cache',
+    'Connection': 'keep - alive',
+    'Content - Length': '101',
+    'Content - Type': 'application / json;charset = UTF - 8',
+   ' Cookie': 'culture = en - US;isCompact = true;_HttpSessionID = 5BF1B10EBD394BD8B5B99ED472BD854E;sysNoticeTypeIds = null',
+    'Culture': 'en - US',
+    'Host': 'www.e - licitatie.ro',
+    'HttpSessionID': 'null',
+    'Origin': 'http: // www.e - licitatie.ro',
+    'Pragma': 'no - cache',
+    'Referer': 'http: // www.e - licitatie.ro / pub / notices / contract - notices / list / 2 / 1RefreshToken: null',
+    'User - Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36'
+    }
+
+    def parse(self, response,**kwargs):
+        yield scrapy.Request(
+            url="http://www.e-licitatie.ro/api-pub/NoticeCommon/GetCNoticeList/",
+            callback=self.parse,
+            headers=self.headers
+        )
+
+    # def parse_json(self, response):
+    #     data = response.json() # Newer version of Scrapy come with shortcut to get JSON data
+    #
+    #     for i,school in enumerate(data):
+    #         school_code = school["itSchoolCode"]
+    #         yield scrapy.Request(
+    #             f"https://directory.ntschools.net/api/System/GetSchool?itSchoolCode={school_code}",
+    #             callback=self.parse_school,
+    #             headers=self.headers,
+    #             dont_filter=True # Many schools have the same code, same page, but listed more than once
+    #         )
+
+    # def parse_school(self, response):
+    #     data = response.json() # Newer version of Scrapy come with shortcut to get JSON data
+    #     yield {
+    #         "name": data["name"],
+    #         "telephoneNumber": data["telephoneNumber"],
+    #         "mail": data["mail"],
+    #         "physicalAddress": data["physicalAddress"]["displayAddress"],
+    #         "postalAddress": data["postalAddress"]["displayAddress"],
+    #     }
+
+
